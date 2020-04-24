4b312603ec909e540ea13b95b5c922e093e0ec62
ddijj <daisuke0221-ddijj@outlook.jp>
2020-04-24 10:44:40 +0900

    ddijj

---
+++ ti.js
@@ -0,0 +1,81 @@
+//上の画像
+var img = document.getElementById("img");
+img.innerHTML = '  <a href="https://ddijj.github.io/" class="samn"><img src="https://ddijj.github.io/サムネ.png"></a>' ;
+//サムネイル
+
+
+//ハンバーガーメニュー
+var a1 = document.getElementById("a1");
+a1.innerHTML = '' ;
+//ハンバーガーメニュー1
+var a2 = document.getElementById("a2");
+a2.innerHTML = '' ;
+//ハンバーガーメニュー2
+var a3 = document.getElementById("a3");
+a3.innerHTML = '' ;
+//ハンバーガーメニュー3
+var a4 = document.getElementById("a4");
+a4.innerHTML = '' ;
+//ハンバーガーメニュー4
+var a5 = document.getElementById("a5");
+a5.innerHTML = '' ;
+//ハンバーガーメニュー5
+var a6 = document.getElementById("a6");
+a6.innerHTML = '' ;
+//ハンバーガーメニュー6
+var a7 = document.getElementById("a7");
+a7.innerHTML = '' ;
+//ハンバーガーメニュー7
+var a8 = document.getElementById("a8");
+a8.innerHTML = '' ;
+//ハンバーガーメニュー8
+var a9 = document.getElementById("a9");
+a9.innerHTML = '' ;
+//ハンバーガーメニュー9
+var a10 = document.getElementById("a10");
+a10.innerHTML = '' ;
+//ハンバーガーメニュー1
+var a11 = document.getElementById("a11");
+a11.innerHTML = '' ;
+//ハンバーガーメニュー11
+var a12 = document.getElementById("a12");
+a12.innerHTML = '' ;
+//ハンバーガーメニュー12
+
+//サイドメニュー
+var b1 = document.getElementById("b1");
+b1.innerHTML =   '' ;
+//サイドメニュー1
+var b2 = document.getElementById("b2");
+b2.innerHTML = '' ;
+//サイドメニュー2
+var b3 = document.getElementById("b2");
+b2.innerHTML = '' ;
+//サイドメニュー3
+var b4 = document.getElementById("b4");
+b4.innerHTML =   '' ;
+//サイドメニュー4
+var b5 = document.getElementById("b5");
+b5.innerHTML = '' ;
+//サイドメニュー5
+var b3 = document.getElementById("b6");
+b6.innerHTML = '' ;
+//サイドメニュー6
+var b7 = document.getElementById("b7");
+b7.innerHTML =   '' ;
+//サイドメニュー7
+var b8 = document.getElementById("b8");
+b9.innerHTML = '' ;
+//サイドメニュー8
+var b9 = document.getElementById("b9");
+b9.innerHTML = '' ;
+//サイドメニュー9
+var b10 = document.getElementById("b10");
+b10.innerHTML =   '' ;
+//サイドメニュー10
+var b11 = document.getElementById("b11");
+b11.innerHTML = '' ;
+//サイドメニュー11
+var b12= document.getElementById("b12");
+b12.innerHTML = '' ;
+//サイドメニュー12
